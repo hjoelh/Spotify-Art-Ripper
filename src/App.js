cdbf58797640732e72999ef2e2cb395366915fb5
hjoelh <jhollingsmith@gmail.com>
2020-09-03 21:11:43 +0100

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+const SpotifyWebApi = require('spotify-web-api-node');
+
 
 function App() {
+
+  const spotifyApi = new SpotifyWebApi();
+
+  const getHashParams = () => {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    while ( e = r.exec(q)) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+  }
+  const params = getHashParams()
+
+
+  const [loggedIn, setLoggedIn] = React.useState(params.access_token ? true : false)
+  const [nowPlaying, setNowPlaying] = React.useState({
+    song: 'Not Checked',
+    artist: 'test',
+    image: ''
+  })
+  const [track, setTrack] = React.useState('')
+
+  if (params.access_token) {
+    spotifyApi.setAccessToken(params.access_token)
+    
+  }
+
+
+  const getNowPlaying = () => {
+    spotifyApi.getTrack('3zmEykbQ6rlvYz4qJHdWMC') 
+      .then(res => {
+        setNowPlaying({
+           artist: res.body.artists[0].name,
+          song: res.body.album.name,
+          image: res.body.album.images[0].url 
+        })
+      })
+  }
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <h1>{loggedIn ? 'Logged in' : 'Logged out'}</h1>
+
+      <a href="http://localhost:8888">
+        <button>Login with Spotify</button>
+      </a>
+
+      <div className='nowplaying'>{nowPlaying.song} by {nowPlaying.artist}</div>
+
+      <div> <img src={nowPlaying.image} alt="album art"/> </div>
+
+
+
+      <div className='nowPlayingBtn' onClick={getNowPlaying}>Click to get now playing</div>
+
+
     </div>
+
   );
 }
 
