33d3d9ecd4124962904b3b613787f80f9bcfc33b
hjoelh <jhollingsmith@gmail.com>
2020-09-05 17:08:18 +0100

    v1.1

--- src/App.js
+++ src/App.js
@@ -1,78 +1,75 @@
 import React from 'react';
-const SpotifyWebApi = require('spotify-web-api-node');
-
+const request = require('request');
 
 function App() {
 
-  const spotifyApi = new SpotifyWebApi();
-
   const regex1 = /(?<=\/).{22}(?=\?)/g
 
-  const getHashParams = () => {
-    var hashParams = {};
-    var e, r = /([^&;=]+)=?([^&;]*)/g,
-        q = window.location.hash.substring(1);
-    while ( e = r.exec(q)) {
-       hashParams[e[1]] = decodeURIComponent(e[2]);
-    }
-    return hashParams;
-  }
-  const params = getHashParams()
-
-
-  const [loggedIn, setLoggedIn] = React.useState(params.access_token ? true : false)
-  const [nowPlaying, setNowPlaying] = React.useState({
-    song: 'Not Checked',
-    artist: 'test',
-    image: ''
-  })
   const [track, setTrack] = React.useState('')
   const [url, setUrl] = React.useState('')
 
-  if (params.access_token) {
-    spotifyApi.setAccessToken(params.access_token)
-    
+  const [nowPlaying, setNowPlaying] = React.useState({
+    song: 'paste a url',
+    artist: 'below',
+    image: ''
+  })
+
+  const URLinput = (e) => {
+    setUrl(e.target.value)
+    console.log(url)
   }
 
-
-  const getNowPlaying = () => {
-    spotifyApi.getTrack(url.match(regex1)) 
-      .then(res => {
-        setNowPlaying({
-           artist: res.body.artists[0].name,
-          song: res.body.album.name,
-          image: res.body.album.images[0].url 
-        })
-      })
-  }
-
-const URLinput = (e) => {
-  setUrl(e.target.value)
-}
+var client_id = process.env.REACT_APP_ID; 
+var client_secret = process.env.REACT_APP_SECRET; 
 
 
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+    },
+    form: {
+      grant_type: 'client_credentials'
+    },
+    json: true
+   };
+   
+   const pingSpotify = ur => {
 
+   request.post(authOptions, function(error, response, body) {
+       if (!error && response.statusCode === 200) {
+      
+         var token = body.access_token;
+         var options = {
+           url: 'https://api.spotify.com/v1/tracks/'+ ur,
+           headers: {
+             'Authorization': 'Bearer ' + token
+           },
+           json: true
+         };
+         request.get(options, function(error, response, body) {
+           setNowPlaying({
+                     artist: body.artists[0].name,
+                    song: body.album.name,
+                    image: body.album.images[0].url 
+                  })
+
+         });
+       }
+      });
+   
+   }
 
   return (
     <div className="App">
-
-    <h1>{loggedIn ? 'Logged in' : 'Logged out'}</h1>
-
-      <a href="http://localhost:8888">
-        <button>Login with Spotify</button>
-      </a>
 
       <div className='nowplaying'>{nowPlaying.song} by {nowPlaying.artist}</div>
 
       <div> <img src={nowPlaying.image} alt="album art"/> </div>
 
-
-
-      <div className='nowPlayingBtn' onClick={getNowPlaying}>Search</div>
-
+      <div className='nowPlayingBtn'  onClick={ () => pingSpotify(url.match(regex1))} >Search</div>
 
       <input placeholder='paste url here' className='nowPlayingBtn' type="text" autoFocus onChange={URLinput} value={url}/>
-   
 
     </div>
 
